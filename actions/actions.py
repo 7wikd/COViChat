a08fb9ec8e59a79b3df3919471a1eb8f7518daf9
7wikd <satwik.dalvi@gmail.com>
2021-03-23 18:14:36 +0530

    Latest

--- actions/actions.py
+++ actions/actions.py
@@ -8,9 +8,16 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, FollowupAction
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import AllSlotsReset
+import smtplib
+import pandas as pd
 import requests
+import json
 
 # class ActionHelloWorld(Action):
 #
@@ -25,44 +32,6 @@ import requests
 #
 #         return []
 
-class ActionHelloWorld(Action):
-    def name(self) -> Text:
-        return "action_hello_world"
-    
-    def run(self, dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        print("Actions.py")
-        dispatcher.utter_message(text="Hello World! First Python action")
-    
-        return []
-
-class ActionSearchRestaurant(Action):
-    def name(self) -> Text:
-        return "action_search_restaurant"
-    
-    def run(self, dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        entities = tracker.latest_message['entities']
-        print(entities)
-
-        for e in entities:
-            if e['entity'] == 'hotel':
-                name = e['value']
-            if name == 'indian':
-                message = "Indian1, Indian2, Indian3"
-            if name == 'chinese':
-                message = "Chi1, Chi2, Chi3"
-            if name == 'Thai':
-                message = "T1, T2, T3"
-            
-        print("Actions.py")
-        dispatcher.utter_message(text=message)
-    
-        return []
 
 class ActionCoronaTracker(Action):
     def name(self) -> Text:
@@ -74,24 +43,35 @@ class ActionCoronaTracker(Action):
         response = requests.get("https://api.covid19india.org/data.json").json()
         
         entities = tracker.latest_message['entities']
-        #print("Last message now...", entities)
-        state = None
+        state = tracker.get_slot("state")
 
-        for e in entities:
-            if e['entity'] == 'state':
-                state = e['value']
         message = "Please enter correct state name: "
 
         if state == "India":
             state = "Total"
-
+ 
         for data in response["statewise"]:
-            if data["state"] == state.title() or data["statecode"]==state.title():
+            if data["state"] == state.title():
                 print(data)
                 if data["statenotes"] == "":
                     data["statenotes"] = "None"
-                message = "As of {}:\nActive: {} Confirmed: {} Recovered: {} Deaths: {}\nAdditional notes:\n{}".format(data['lastupdatedtime'],data['active'],data['confirmed'],data['recovered'],data['deaths'],data['statenotes'])
+                message = "Hello from COVID-tracker! Current conditions for {} are:\nAs of {}:\nActive: {} Confirmed: {} Recovered: {} Deaths: {}\nAdditional notes:\n{}"\
+                    .format(state.title(),data['lastupdatedtime'],data['active'],data['confirmed'],data['recovered'],data['deaths'],data['statenotes'])
+                
         
-        dispatcher.utter_message(text=f"Hello from COVID-tracker! Current conditions for {state.title()} are:\n" + message)
+        dispatcher.utter_message(text=message)
     
         return []
+
+def send_Email(emailid, name, body):
+    s = stplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+
+    s.login("satwik.dalvi@gmail.com", "Itwillbeonyounowson1123")
+
+    text = "Dear {},\n\n" "Hereby find the required details:\n" + body + "\n\nThanks and regards,\n --COViChat"
+
+    subject = "COViChat request summary"
+    message = 'Subject:{}\n\n{}'.format(subject,text)
+    s.sendmail("satwik.dalvi@gmail.com",emailid,message)
+    s.quit()
